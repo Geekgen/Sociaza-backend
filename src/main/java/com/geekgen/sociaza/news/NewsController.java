76c5102974f1a1b98f428edf0e2495ccec734267
Denise Muniz <dendenmuniz@gmail.com>
2020-07-21 16:08:54 +0200

    improve news api to call more news types

--- src/main/java/com/geekgen/sociaza/news/NewsController.java
+++ src/main/java/com/geekgen/sociaza/news/NewsController.java
@@ -3,11 +3,13 @@ package com.geekgen.sociaza.news;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
@@ -17,12 +19,10 @@ public class NewsController {
     private NewsService newsService;
 
 
-
-
     @GetMapping(path = "/news",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    private ResponseEntity<List<Article>> getNews(){
-        List<Article> news = newsService.getArticles();
+    private ResponseEntity<List<Article>> getNews(@RequestParam(name = "type", required = false) String type){
+        List<Article> news = newsService.getArticles(type);
         if (news != null) {
             return new ResponseEntity<List<Article>>(news, HttpStatus.OK);
         } else {
--- src/main/java/com/geekgen/sociaza/news/NewsService.java
+++ src/main/java/com/geekgen/sociaza/news/NewsService.java
@@ -15,21 +15,44 @@ public class NewsService {
     @Value("${spring.newsapi.api.url}")
     private String apiUrl;
 
+    @Value("${spring.newsapi.api.key}")
+    private String apiKey;
+
     @Autowired
     RestTemplate restTemplate;
 
-    public NewsApi getAllNews(){
+    public NewsApi getAllNews(String newsType){
+        String urlApiFinal = new String();
+        switch(newsType) {
+            case "general":
+                urlApiFinal = apiUrl +  "country=br&category=general&" + apiKey;
+                break;
+            case "sport":
+                urlApiFinal = apiUrl +  "country=br&category=sport&" + apiKey;
+                break;
+            case "health":
+                urlApiFinal = apiUrl +  "country=br&category=health&" + apiKey;
+                break;
+            case "entertainment":
+                urlApiFinal = apiUrl +  "country=br&category=entertainment&" + apiKey;
+                break;
+            case "technology":
+                urlApiFinal = apiUrl +  "country=br&category=technology&" + apiKey;
+                break;
+            case "se":
+                urlApiFinal = apiUrl +  "country=se&category=general&" + apiKey;
+                break;
+            default:
+                urlApiFinal = apiUrl +  "country=br&category=general&" + apiKey;
 
-        return restTemplate.getForObject(apiUrl, NewsApi.class);
+        }
 
-     //   NewsApi call= restTemplate.getForObject(apiUrl, NewsApi.class);
-     //   return call;
-        //System.out.println(call.getBody())
-       // return this.restTemplate.getForObject(apiUrl, NewsApi.class);
+        return restTemplate.getForObject(urlApiFinal, NewsApi.class);
     }
 
-    public List<Article> getArticles(){
-        NewsApi news = getAllNews();
+
+    public List<Article> getArticles(String newsType){
+        NewsApi news = getAllNews(newsType);
         if (news != null) return news.getArticles();
         return null;
     }
